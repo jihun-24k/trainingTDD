e3dcd5829f0f83c4e19a3fc780b74f22340c7c8c
jihun-24k <gi020477@gmail.com>
2022-07-05 13:49:20 +0900

    Feat: add 메서드 run메서드로 메서명 변경

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class Calculator {
-    public static int add(String data){
+    public static int run(String data){
         String[] dataBits = data.split(" \\+ ");
         int n1 = Integer.parseInt(dataBits[0]);
         int n2 = Integer.parseInt(dataBits[1]);
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -7,19 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void addTest1(){
-        int rs = Calculator.add("10 + 10");
+        int rs = Calculator.run("10 + 10");
         assertEquals(20, rs);
     }
 
     @Test
     public void addTest2(){
-        int rs = Calculator.add("20 + 20");
+        int rs = Calculator.run("20 + 20");
         assertEquals(40,rs);
     }
 
     @Test
     public void addTest4(){
-        int rs = Calculator.add("30 + 20");
+        int rs = Calculator.run("30 + 20");
         assertEquals(50, rs);
     }
 }
