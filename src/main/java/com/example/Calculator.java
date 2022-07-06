19f7cff0791aa70d9f4f56a1fcb88d6d051efe36
jihun-24k <gi020477@gmail.com>
2022-07-06 22:57:25 +0900

    Feat: run 함수명 변경

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,13 +1,14 @@
 package com.example;
 
 public class Calculator {
-    public static int run(String data){
+    public static int calc(String data){
         String[] dataBits = data.split(" ");
         int n1 = Integer.parseInt(dataBits[0]);
         String operation1 = dataBits[1];
         int n2 = Integer.parseInt(dataBits[2]);
         String operation2 = dataBits[3];
         int n3 = Integer.parseInt(dataBits[4]);
+
 
         int result = 0;
 
@@ -22,15 +23,6 @@ public class Calculator {
             result = n1/n2;
         }
 
-        if (operation2.equals("+")){
-            result += n3;
-        } else if (operation2.equals("-")){
-            result -= n3;
-        }else if (operation1.equals("*")){
-            result *= n3;
-        }else if(operation1.equals("/")){
-            result /= n3;
-        }
         return result;
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -7,49 +7,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void addTest1(){
-        int rs = Calculator.run("10 + 10");
+        int rs = Calculator.calc("10 + 10");
         assertEquals(20, rs);
     }
 
     @Test
     public void addTest2(){
-        int rs = Calculator.run("20 + 20");
+        int rs = Calculator.calc("20 + 20");
         assertEquals(40,rs);
     }
 
     @Test
     public void addTest4(){
-        int rs = Calculator.run("30 + 20");
+        int rs = Calculator.calc("30 + 20");
         assertEquals(50, rs);
     }
 
     @Test
     public void minusTest1(){
-        int rs = Calculator.run("30 - 20");
+        int rs = Calculator.calc("30 - 20");
         assertEquals(10,rs);
     }
 
     @Test
     public void minusTest2(){
-        int rs = Calculator.run("40 - 20");
+        int rs = Calculator.calc("40 - 20");
         assertEquals(20,rs);
     }
 
     @Test
     public void multipleTest1(){
-        int rs = Calculator.run("20 * 20");
+        int rs = Calculator.calc("20 * 20");
         assertEquals(400, rs);
     }
 
     @Test
     public void divideTest1(){
-        int rs = Calculator.run("30 / 10");
+        int rs = Calculator.calc("30 / 10");
         assertEquals(3, rs);
     }
 
     @Test
     public void polynomialTest1(){
-        int rs = Calculator.run("1 + 2 + 3");
+        int rs = Calculator.calc("1 + 2 + 3");
         assertEquals(6, rs);
     }
 }
